5895e2dd86166924b1134f60d4b72aa7c4ac2968
Yudis Setyawan <acer_trafelmate@yahoo.co.id>
2023-05-22 04:22:10 +0000

    first commit

---
+++ js/script.js
@@ -0,0 +1,36 @@
+//Create function to select elements
+const selectElement = (element) => document.querySelector(element);
+//Open and close nav on click
+selectElement(".menu-icons").addEventListener("click", () => {
+  selectElement("nav").classList.toggle("active");
+});
+
+var swiper = new Swiper(".slide-container", {
+  slidesPerView: 3,
+  spaceBetween: 25,
+  loop: true,
+  centerSlide: "true",
+  fade: "true",
+  grabCursor: "true",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+    950: {
+      slidesPerView: 3,
+    },
+  },
+});
